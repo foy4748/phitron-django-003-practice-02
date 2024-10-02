c54da4a0ae35ccef3f6c67e6ac22bbf556e82808
foy4748 <faisaljfcl@gmail.com>
2024-10-02 23:43:42 +0600

    Converted Album app to class-based views

--- album/urls.py
+++ album/urls.py
@@ -3,8 +3,12 @@ from album import views
 
 app_name = "album"
 urlpatterns = [
-    path("", views.ShowAlbums, name="all_albums"),
-    path("add/", views.ShowAddAlbumForm, name="add_album_form"),
-    path("edit/<str:pk>", views.EditAlbum, name="edit_album"),
-    path("delete/<str:pk>", views.DeleteAlbum, name="delete_album"),
+    path("", views.ShowAlbums.as_view(), name="all_albums"),
+    path("add/", views.ShowAddAlbumForm.as_view(), name="add_album_form"),
+    path("edit/<str:pk>", views.EditAlbum.as_view(), name="edit_album"),
+    path("delete/<str:pk>", views.DeleteAlbum.as_view(), name="delete_album"),
+    # path("", views.ShowAlbums, name="all_albums"),
+    # path("add/", views.ShowAddAlbumForm, name="add_album_form"),
+    # path("edit/<str:pk>", views.EditAlbum, name="edit_album"),
+    # path("delete/<str:pk>", views.DeleteAlbum, name="delete_album"),
 ]
--- album/views.py
+++ album/views.py
@@ -1,37 +1,62 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic.list import ListView
+from django.urls import reverse_lazy
 
 from album.forms import AddAlbumForm
 from album.models import Album
+from django.views.generic import CreateView
+from django.views.generic.edit import UpdateView, DeleteView
 
 
 # Create your views here.
-def ShowAddAlbumForm(req):
-    if req.POST:
-        current_form = AddAlbumForm(req.POST)
-        if current_form.is_valid():
-            current_form.save()
-            return redirect("album:all_albums")
-    form = AddAlbumForm()
-    return render(req, "album/add_album_form.html", {"form": form})
+class ShowAddAlbumForm(CreateView):
+    model = Album
+    form_class = AddAlbumForm
+    success_url = reverse_lazy("album:all_albums")
 
 
-def EditAlbum(req, pk):
-    album_instance = get_object_or_404(Album, pk=pk)
-    if req.POST:
-        current_form = AddAlbumForm(req.POST, instance=album_instance)
-        if current_form.is_valid():
-            current_form.save()
-            return redirect("album:all_albums")
-    form = AddAlbumForm(instance=album_instance)
-    return render(req, "album/add_album_form.html", {"form": form})
+class EditAlbum(UpdateView):
+    model = Album
+    form_class = AddAlbumForm
+    success_url = reverse_lazy("album:all_albums")
 
 
-def DeleteAlbum(_, pk):
-    album_instance = get_object_or_404(Album, pk=pk)
-    album_instance.delete()
-    return redirect("album:all_albums")
+class DeleteAlbum(DeleteView):
+    model = Album
+    success_url = reverse_lazy("album:all_albums")
 
 
-def ShowAlbums(req):
-    albums = Album.objects.all()
-    return render(req, "album/albums.html", {"albums": albums})
+class ShowAlbums(ListView):
+    model = Album
+
+
+# def ShowAddAlbumForm(req):
+#     if req.POST:
+#         current_form = AddAlbumForm(req.POST)
+#         if current_form.is_valid():
+#             current_form.save()
+#             return redirect("album:all_albums")
+#     form = AddAlbumForm()
+#     return render(req, "album/add_album_form.html", {"form": form})
+
+
+# def EditAlbum(req, pk):
+#     album_instance = get_object_or_404(Album, pk=pk)
+#     if req.POST:
+#         current_form = AddAlbumForm(req.POST, instance=album_instance)
+#         if current_form.is_valid():
+#             current_form.save()
+#             return redirect("album:all_albums")
+#     form = AddAlbumForm(instance=album_instance)
+#     return render(req, "album/add_album_form.html", {"form": form})
+
+
+# def DeleteAlbum(_, pk):
+#     album_instance = get_object_or_404(Album, pk=pk)
+#     album_instance.delete()
+#     return redirect("album:all_albums")
+
+
+# def ShowAlbums(req):
+#     albums = Album.objects.all()
+#     return render(req, "album/albums.html", {"albums": albums})
